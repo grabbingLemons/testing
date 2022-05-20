b2dc2b755fe76599d88c2ee48e1a27f7c1d5f061
SimpleExistance <sherdog48@gmail.com>
2022-05-20 01:33:18 -0500

    removed unnessary files

--- kl2_chatfix.lua
+++
@@ -1,7 +0,0 @@
-if not game:IsLoaded() then
-game.Loaded:Wait()
-end
-while true do
-    game:GetService("StarterGui"):SetCore("TopbarEnabled", true)    
-    wait(.1)
-end
--- kl2_defog.lua
+++
@@ -1,6 +0,0 @@
-if not game:IsLoaded() then
-game.Loaded:Wait()
-end
-local Part = game:GetService("Lighting").DayAtmosphere
-Part:Destroy()
-Part.Parent = game:GetService("Lighting")
